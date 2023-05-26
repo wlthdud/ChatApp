bfe37aaba2472cc9bae7e1e222a6467d23cde444
wlthdud <thdudzi@naver.com>
2023-05-27 04:38:45 +0900

    feat: firebase, bootstrap download

---
+++ chatapp/src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyANFG0QVuSVK78nZgvQG_MM3Ggm_Fu7E5o",
+    authDomain: "firechat-ff60a.firebaseapp.com",
+    projectId: "firechat-ff60a",
+    storageBucket: "firechat-ff60a.appspot.com",
+    messagingSenderId: "1028737881996",
+    appId: "1:1028737881996:web:06b2f376b981391a92e8a9",
+    measurementId: "G-KEH6T24TJB"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
