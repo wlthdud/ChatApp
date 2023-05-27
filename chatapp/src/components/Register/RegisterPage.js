797e8501e6173a5c594053e0e6b829461841c867
wlthdud <thdudzi@naver.com>
2023-05-27 20:41:48 +0900

    feat: firebase에 생성한 유저에 상세 정보 추가하기

--- chatapp/src/components/Register/RegisterPage.js
+++ chatapp/src/components/Register/RegisterPage.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import {Link, BrowserRouter} from 'react-router-dom';
 import {useForm} from 'react-hook-form';
 import firebase from '../../firebase';
+import md5 from 'md5';
 
 function RegisterPage() {
     
@@ -20,6 +21,12 @@ function RegisterPage() {
                 .auth()
                 .createUserWithEmailAndPassword(data.email, data.password);
             console.log('createdUser', createdUser);
+
+            await createdUser.user.updateProfile({
+                displayName :data.name,
+                photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`   
+            })
+
             setLoading(false);
         }catch(error){
             setErrorFromSubmit(error.message);
