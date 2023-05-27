356ea65a2997f35cd47d419401931ff2750239a5
wlthdud <thdudzi@naver.com>
2023-05-27 20:05:50 +0900

    feat: firebase에서 이메일로 유저 생성시 생기는 문제점

--- chatapp/src/components/Register/RegisterPage.js
+++ chatapp/src/components/Register/RegisterPage.js
@@ -1,62 +1,79 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import {Link, BrowserRouter} from 'react-router-dom';
 import {useForm} from 'react-hook-form';
+import firebase from '../../firebase';
 
 function RegisterPage() {
     
-    const {register, watch, formState: {errors}} = useForm();
+    const {register, watch, formState: {errors}, handleSubmit} = useForm();
+    const [errorFromSubmit, setErrorFromSubmit] = useState("");
 
     const password = useRef();
     password.current = watch("password");
 
-    console.log('password.current', password.current);
+    const onSubmit = async (data) => {
+
+        try{
+            let createdUser = await firebase
+                .auth()
+                .createUserWithEmailAndPassword(data.email, data.password);
+            console.log('createdUser', createdUser);
+        }catch(error){
+            setErrorFromSubmit(error.message);
+            setTimeout(()=>{
+                setErrorFromSubmit("");
+            },5000);
+        }
+    }
 
     return (
         <div className='auth-Wrapper'>
             <div style={{textAlign:"center"}}>
                 <h3>Register</h3>
             </div>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
             
-            <label>Email</label>
-            <input
-                name="email"
-                type="email"
-                {...register("email", {required: true, pattern: /^\S+@\S+$/i})}
-            />
-            {errors.email && <p>This email field is required</p>}
-    
-            <label>Name</label>
-            <input
-                name="name"
-                {...register("name", {required: true, maxLength: 10})}
-            />
-            {errors.name && errors.name.type === "required" && <p>This name field is required</p>}
-            {errors.name && errors.name.type === "maxLength" && <p>Your input exceed maximum length</p>}
-    
-            <label>Password</label>
-            <input
-                name="password"
-                type="password"
-                {...register("password", {required:true, minLength:6})}
-            />
-            {errors.password && errors.password.type === "required" && <p>This password field is required</p>}
-            {errors.password && errors.password.type === "minLength" && <p>Password must have at least 6 characters</p>}
-    
-            <label>Password Confirm</label>
-            <input
-                name="password_confirm"
-                type="password"
-                {...register("password_confirm", {
-                  required:true, 
-                  validate: (value) =>
-                    value === password.current
-                })}
-            />
-            {errors.password_confirm && errors.password_confirm.type === "required" && <p>This password confirm field is required</p>}
-            {errors.password_confirm && errors.password_confirm.type === "validate" && <p>The password do not match</p>}
-    
-            <input type="submit" />
+                <label>Email</label>
+                <input
+                    name="email"
+                    type="email"
+                    {...register("email", {required: true, pattern: /^\S+@\S+$/i})}
+                />
+                {errors.email && <p>This email field is required</p>}
+        
+                <label>Name</label>
+                <input
+                    name="name"
+                    {...register("name", {required: true, maxLength: 10})}
+                />
+                {errors.name && errors.name.type === "required" && <p>This name field is required</p>}
+                {errors.name && errors.name.type === "maxLength" && <p>Your input exceed maximum length</p>}
+        
+                <label>Password</label>
+                <input
+                    name="password"
+                    type="password"
+                    {...register("password", {required:true, minLength:6})}
+                />
+                {errors.password && errors.password.type === "required" && <p>This password field is required</p>}
+                {errors.password && errors.password.type === "minLength" && <p>Password must have at least 6 characters</p>}
+        
+                <label>Password Confirm</label>
+                <input
+                    name="password_confirm"
+                    type="password"
+                    {...register("password_confirm", {
+                    required:true, 
+                    validate: (value) =>
+                        value === password.current
+                    })}
+                />
+                {errors.password_confirm && errors.password_confirm.type === "required" && <p>This password confirm field is required</p>}
+                {errors.password_confirm && errors.password_confirm.type === "validate" && <p>The password do not match</p>}
+        
+                    {errorFromSubmit && <p>{errorFromSubmit}</p>}
+
+                <input type="submit" />
             </form>
             <BrowserRouter>
               <Link style={{color:"gray", textDecoration:"none"}} to="login">이미 아이디가 있다면...</Link>
--- chatapp/src/firebase.js
+++ chatapp/src/firebase.js
@@ -1,7 +1,8 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/database";
-import "firebase/storage";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/database";
+import "firebase/compat/storage";
+import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,3 +19,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
+
+export default firebase;
