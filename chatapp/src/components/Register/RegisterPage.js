dc5547904a348f16df24eaedf5254a669397d443
wlthdud <thdudzi@naver.com>
2023-05-27 21:05:23 +0900

    feat: firebase에 생성한 유저 데이터베이스에 저장

--- chatapp/src/components/Register/RegisterPage.js
+++ chatapp/src/components/Register/RegisterPage.js
@@ -27,6 +27,12 @@ function RegisterPage() {
                 photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`   
             })
 
+            //Firebase 데이터베이스에 저장해주기
+            await firebase.database().ref("users").child(createdUser.user.uid).set({
+                name: createdUser.user.displayName,
+                image: createdUser.user.photoURL
+            })
+
             setLoading(false);
         }catch(error){
             setErrorFromSubmit(error.message);
